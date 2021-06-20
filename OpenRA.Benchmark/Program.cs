14a44a2d0ee5dfc71a911a2a895662789d8c23e9
teinarss <einarsson.tomas@gmail.com>
2021-06-20 21:34:32 +0200

    ooa

---
+++ OpenRA.Benchmark/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using OpenRA.Primitives;
+
+namespace OpenRA.Benchmark
+{
+    class Program
+    {
+	    public static void Main(string[] args) => BenchmarkRunner.Run<ActionQueueBenchmark>();
+    }
+
+    public class ActionQueueBenchmark
+    {
+
+	    ActionQueue actionQueue = new ActionQueue();
+	    ActionQueue1 actionQueue1 = new ActionQueue1();
+
+
+[Benchmark]
+	    public void Foo1() => actionQueue.PerformActions(1);
+
+	    [Benchmark]
+	    public void Foo2() => actionQueue1.PerformActions(1);
+
+}
+}
